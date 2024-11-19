6b733a0f7abe79395449ed87819ee075120e22bb
nopnop2002 <hau26280@tree.odn.ne.jp>
2024-11-20 06:42:03 +0900

    changed TAB

--- main/st7735s.h
+++ main/st7735s.h
@@ -6,15 +6,15 @@
 
 #define rgb565(r, g, b) (((r & 0xF8) << 8) | ((g & 0xFC) << 3) | (b >> 3))
 
-#define RED    rgb565(255,	 0,   0) // 0xf800
-#define GREEN  rgb565(	0, 255,   0) // 0x07e0
-#define BLUE   rgb565(	0,	 0, 255) // 0x001f
-#define BLACK  rgb565(	0,	 0,   0) // 0x0000
+#define RED    rgb565(255, 000, 000) // 0xf800
+#define GREEN  rgb565(000, 255, 000) // 0x07e0
+#define BLUE   rgb565(000, 000, 255) // 0x001f
+#define BLACK  rgb565(000, 000, 000) // 0x0000
 #define WHITE  rgb565(255, 255, 255) // 0xffff
 #define GRAY   rgb565(128, 128, 128) // 0x8410
-#define YELLOW rgb565(255, 255,   0) // 0xFFE0
-#define CYAN   rgb565(	0, 156, 209) // 0x04FA
-#define PURPLE rgb565(128,	 0, 128) // 0x8010
+#define YELLOW rgb565(255, 255, 000) // 0xFFE0
+#define CYAN   rgb565(000, 156, 209) // 0x04FA
+#define PURPLE rgb565(128, 000, 128) // 0x8010
 
 typedef enum {DIRECTION0, DIRECTION90, DIRECTION180, DIRECTION270} DIRECTION;
 
@@ -69,5 +69,6 @@ void lcdUnsetFontFill(ST7735_t * dev);
 void lcdSetFontUnderLine(ST7735_t * dev, uint16_t color);
 void lcdUnsetFontUnderLine(ST7735_t * dev);
 void lcdDrawFinish(ST7735_t *dev);
+
 #endif /* MAIN_ST7735_H_ */
 
