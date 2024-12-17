03762cd88682007dfa4c41c0ec1c0d2df2a29996
nopnop2002 <hau26280@tree.odn.ne.jp>
2024-12-17 10:09:04 +0900

    Changed spiffs mount options

--- main/main.c
+++ main/main.c
@@ -988,81 +988,72 @@ void tft(void *pvParameters)
 	}
 }
 
-esp_err_t mountSPIFFS(char * partition_label, char * mount_point) {
-	ESP_LOGI(TAG, "Initializing SPIFFS");
+static void listSPIFFS(char * path) {
+	DIR* dir = opendir(path);
+	assert(dir != NULL);
+	while (true) {
+		struct dirent*pe = readdir(dir);
+		if (!pe) break;
+		ESP_LOGI(__FUNCTION__,"d_name=%s d_ino=%d d_type=%x", pe->d_name,pe->d_ino, pe->d_type);
+	}
+	closedir(dir);
+}
+
+esp_err_t mountSPIFFS(char * path, char * label, int max_files) {
 	esp_vfs_spiffs_conf_t conf = {
-		.base_path = mount_point,
-		.partition_label = partition_label,
-		.max_files = 10, // maximum number of files which can be open at the same time
-		.format_if_mount_failed = true
+		.base_path = path,
+		.partition_label = label,
+		.max_files = max_files,
+		.format_if_mount_failed =true
 	};
 
-	// Use settings defined above to initialize and mount SPIFFS filesystem.
-	// Note: esp_vfs_spiffs_register is an all-in-one convenience function.
+	// Use settings defined above toinitialize and mount SPIFFS filesystem.
+	// Note: esp_vfs_spiffs_register is anall-in-one convenience function.
 	esp_err_t ret = esp_vfs_spiffs_register(&conf);
 
 	if (ret != ESP_OK) {
-		if (ret == ESP_FAIL) {
+		if (ret ==ESP_FAIL) {
 			ESP_LOGE(TAG, "Failed to mount or format filesystem");
-		} else if (ret == ESP_ERR_NOT_FOUND) {
+		} else if (ret== ESP_ERR_NOT_FOUND) {
 			ESP_LOGE(TAG, "Failed to find SPIFFS partition");
 		} else {
-			ESP_LOGE(TAG, "Failed to initialize SPIFFS (%s)", esp_err_to_name(ret));
+			ESP_LOGE(TAG, "Failed to initialize SPIFFS (%s)",esp_err_to_name(ret));
 		}
 		return ret;
 	}
 
-	size_t total = 0, used = 0;
-	//ret = esp_spiffs_info(NULL, &total, &used);
-	ret = esp_spiffs_info(partition_label, &total, &used);
+#if 0
+	ESP_LOGI(TAG, "Performing SPIFFS_check().");
+	ret = esp_spiffs_check(conf.partition_label);
 	if (ret != ESP_OK) {
-		ESP_LOGE(TAG, "Failed to get SPIFFS partition information (%s)", esp_err_to_name(ret));
+		ESP_LOGE(TAG, "SPIFFS_check() failed (%s)", esp_err_to_name(ret));
+		return ret;
 	} else {
-		ESP_LOGI(TAG, "Partition [%s] size: total: %d, used: %d", mount_point, total, used);
+			ESP_LOGI(TAG, "SPIFFS_check() successful");
 	}
+#endif
+
+	size_t total = 0, used = 0;
+	ret = esp_spiffs_info(conf.partition_label, &total, &used);
+	if (ret != ESP_OK) {
+		ESP_LOGE(TAG,"Failed to get SPIFFS partition information (%s)",esp_err_to_name(ret));
+	} else {
+		ESP_LOGI(TAG,"Mount %s to %s success", path, label);
+		ESP_LOGI(TAG,"Partition size: total: %d, used: %d", total, used);
+	}
+
 	return ret;
-}
-
-static int getFileSize(char *fullPath) {
-	struct stat st;
-	if (stat(fullPath, &st) == 0)
-		return st.st_size;
-	return -1;
-}
-
-static void printDirectory(char * path) {
-	DIR* dir = opendir(path);
-	assert(dir != NULL);
-	while (true) {
-		struct dirent *pe = readdir(dir);
-		if (!pe) break;
-		if (pe->d_type == 1) {
-			char fullPath[64];
-			strcpy(fullPath, path);
-			strcat(fullPath, "/");
-			strcat(fullPath, pe->d_name);
-			int fsize = getFileSize(fullPath);
-			ESP_LOGI(__FUNCTION__,"%s d_name=%s d_ino=%d fsize=%d", path, pe->d_name, pe->d_ino, fsize);
-		}
-		if (pe->d_type == 2) {
-			char subDir[127];
-			sprintf(subDir,"%s%.64s", path, pe->d_name);
-			ESP_LOGI(TAG, "subDir=[%s]", subDir);
-			printDirectory(subDir);
-
-		}
-	}
-	closedir(dir);
 }
 
 void app_main(void)
 {
 	// Mount SPIFFS File System on FLASH
 	ESP_LOGI(TAG, "Initializing SPIFFS");
-	ESP_ERROR_CHECK(mountSPIFFS("storage1", "/fonts"));
-	printDirectory("/fonts");
-	ESP_ERROR_CHECK(mountSPIFFS("storage2", "/images"));
-	printDirectory("/images");
+	ESP_ERROR_CHECK(mountSPIFFS("/fonts", "storage1", 8));
+	listSPIFFS("/fonts");
+
+	ESP_ERROR_CHECK(mountSPIFFS("/images", "storage2", 1));
+	listSPIFFS("/images");
 
 	// power on
 	i2c_master_init();
